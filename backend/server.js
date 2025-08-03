af43db9d9cf63bc018003c5315bdb0495ea40272
Your Name <you@example.com>
2025-08-03 17:58:55 +0530

    backedn code

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+app.use(cors());
+
+io.on('connection', socket => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('light', data => {
+    console.log('Broadcasting light:', data);
+    socket.broadcast.emit('light', data);
+  });
+   socket.on('binary', data => {
+    console.log('Broadcasting binary:', data);
+    socket.broadcast.emit('binary', data);
+  });
+});
+
+server.listen(3000, () => console.log('Socket server running on http://localhost:3000'));
