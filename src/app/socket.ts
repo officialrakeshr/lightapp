2ef7ecdce64313ab908947037eed0aca8d81cc93
Your Name <you@example.com>
2025-08-08 23:18:53 +0530

    push refresh mode

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -11,13 +11,31 @@ export class SocketService {
   public binaryFlow$ = new BehaviorSubject<{ sequence: string, color: string, interval: number }>({ sequence: '', color: '#000', interval: 1000 });
   public location = inject(Location)
   constructor() {
-    this.socket = io(`${window.location.protocol}//${window.location.hostname}:3000`);
+    this.socket = io(`${window.location.protocol}//${window.location.hostname}:3000`, {
+      reconnectionAttempts: 1,  // Try reconnecting 1 times
+      reconnectionDelay: 2000   // Wait 2s between tries
+    });
     this.socket.on('light', (data: any) => {
       this.color$.next(data.color);
       this.effect$.next(data.effect);
     });
     this.socket.on('binary', (data: any) => {
       this.binaryFlow$.next(data);
+    });
+
+    this.socket.on('disconnect', (reason) => {
+      console.warn('⚠️ Socket disconnected:', reason);
+
+      // If reconnect fails completely, refresh page
+      if (reason === 'io server disconnect') {
+        // Server forced disconnect → no auto reconnect
+        window.location.reload();
+      }
+    });
+
+    this.socket.io.on('reconnect_failed', () => {
+      console.error('❌ Reconnect failed — refreshing...');
+      window.location.reload();
     });
   }
 
@@ -27,5 +45,5 @@ export class SocketService {
   emitBinaryFlow(sequence: string, color: string, interval: number) {
     this.socket.emit('binary', { sequence, color, interval });
   }
-  
+
 }
